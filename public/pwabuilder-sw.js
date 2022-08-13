d6d55ecd67e7e1cc77d43d7ab7c5da5dac5671c4
Alexander Sadovoy <alexander.sadovoy@here.com>
2022-08-13 17:21:19 +0200

    CHG

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,6 +1,6 @@
 //This is the service worker with the Advanced caching
 
-const CACHE = "pwabuilder-adv-cache-v0.4.0-4";
+const CACHE = "pwabuilder-adv-cache-v0.4.0-5";
 const precacheFiles = [
     /* Add an array of files to precache for your app */
     './',
