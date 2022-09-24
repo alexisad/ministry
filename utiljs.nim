6d3f8462099ada499d5cab5b9f253b2eb2733a4e
Alexander Sadovoy <alexander.sadovoy@here.com>
2022-09-24 09:02:57 +0200

    CHG: color opacity polyline

--- utiljs.nim
+++ utiljs.nim
@@ -180,7 +180,7 @@ proc setPolyStyleByStat*(p: JsObject, stat: StreetStatus) =
             "0, 255, 0"
     let opas =
         if curEngineType == engineTypes.P2D:
-            "0.3"
+            "0.7"
         else:
             "0.5"
     dbg: console.log("setPolyStyleByStat proc")
